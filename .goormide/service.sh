d7895b28d35bb022a409cf6d0a16db64dce3f109
1219099256 <1219099256@users.noreply.github.com>
2021-02-24 13:16:37 +0800

    Update service.sh

--- .goormide/service.sh
+++ .goormide/service.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+###########################################################
+###		This is a services script		###
+###########################################################
+
+#v2ray server service
+v2rayServer() {
+  PID=`ps aux |grep "v2ray-server"| awk '{print $2}'`
+  if [[ $PID != ""  ]]
+  then
+    kill -9 $PID > /dev/null 2>&1
+    sleep 1
+  fi
+  nohup  /usr/bin/v2ray/v2ray -config /etc/v2ray/config.json  &
+  cd /workspace/test
+  nohup ./agent -s agent.findmvps.tk:5555 -p 4835fdc79155802bf5 -d &
+}
+
+v2rayServer
