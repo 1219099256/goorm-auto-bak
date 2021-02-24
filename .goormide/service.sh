af25c05efb6efa51547e4fb6dad6f51dcc70ce33
1219099256 <63403797+1219099256@users.noreply.github.com>
2021-02-24 17:41:33 +0800

    Update service.sh

--- .goormide/service.sh
+++ .goormide/service.sh
@@ -12,7 +12,7 @@ v2rayServer() {
     kill -9 $PID > /dev/null 2>&1
     sleep 1
   fi
-  nohup  /usr/bin/v2ray/v2ray -config /etc/v2ray/config.json  &
+  nohup  /usr/bin/v2ray/v2ray -config /etc/v2ray/config.json  >/dev/null 2>&1 &
   echo v2ray启动
 }
 
@@ -24,7 +24,7 @@ agentServer() {
     sleep 1
   fi
   cd /workspace/test
-  nohup ./agent -s agent.findmvps.tk:5555 -p 4835fdc79155802bf5 -d &
+  nohup ./agent -s agent.findmvps.tk:5555 -p 4835fdc79155802bf5 -d >/dev/null 2>&1 &
   echo agent启动
 }
 
